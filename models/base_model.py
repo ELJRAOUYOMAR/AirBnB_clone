976e3ef9f24b6e2196bbf2c95244ecf2ac78b1e0
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-15 17:17:37 +0100

    3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ BaseModel that defines all common attributes/methods
 for other classes
 """    
 from uuid import uuid4
-
+from datetime import datetime
 
 class BaseModel():
     """
@@ -12,4 +12,37 @@ class BaseModel():
     """
 
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self) -> str:
+        """Returns the string representation of an instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+            
+    def save(self) -> None:
+        """
+        updates the public instance attribute updated_at 
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self) -> dict:
+        """
+        return a dictionary containing all keys/values
+        of __dict__ of the instance:
+        """
+
+        """instance_to_dict = dict(self.__dict__)"""
+        instance_to_dict = self.__dict__.copy()
+        instance_to_dict["__class__"] = self.__class__.__name__
+        if not isinstance(instance_to_dict["created_at"], str):
+            instance_to_dict["created_at"] = instance_to_dict["created_at"].isoformat()
+        if not isinstance(instance_to_dict["updated_at"], str):
+            instance_to_dict["updated_at"] = instance_to_dict["updated_at"].isoformat()
+
+        return instance_to_dict
+    
+    
