d77756d2a1581ce7dcc464dd035ba85e33d0d988
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-15 20:33:52 +0100

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,24 @@ class BaseModel():
     define all common attributes/methods for other classes
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel.
+
+        Args:
+            *args: Unused positional arguments.
+            **kwargs: Dictionary of attributes to initialize the instance.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
