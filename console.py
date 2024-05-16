2e721a2e453e22200a0266d3b6c370d2d9b2a1c0
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-16 22:31:33 +0100

    6. Console 0.0.1

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, argument):
+        """command to exit the program"""
+        return True
+    
+    def do_EOF (self, argument):
+        """EOF (Ctrl+D) signal to exit the program"""
+        return True
+
+    def empty_line(self):
+        """empty line + ENTER shouldn’t execute anything"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
