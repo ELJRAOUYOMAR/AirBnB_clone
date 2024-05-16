6500c0212c6fd6548a5524f4aab7321e0cd87ba0
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-16 14:15:17 +0100

    5. Store first object-class FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -20,17 +20,16 @@ class BaseModel():
             *args: Unused positional arguments.
             **kwargs: Dictionary of attributes to initialize the instance.
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
+            
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
         return "[{}] ({}) {}".format(
---
+++ models/engine/file_storage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+convert the dictionary representation to a JSON string.
+JSON is a standard representation of a data structure.
+With this format, humans can read and all programming languages
+have a JSON reader and writer.
+"""
+import json 
+import os
+
+class FileStorage:
+    """
+    serializes instances to a JSON file 
+    and deserializes JSON file to instances
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self) -> dict:
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
+        key = f"{obj.__class__.__name}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
+        with open(self.__file_path, 'w') as f:
+            json.dump(FileStorage.__objects, f)
+
+    def reload(self):
+        """
+        deserializes the JSON file to __objects (only if the JSON file (__file_path) 
+        exists ; otherwise, do nothing. If the file doesn’t exist,
+        no exception should be raised)
+        """
+        file_path = FileStorage.__file_path
+        if os.path.exists(file_path):
+            try:
+                with open(file_path, encoding="utf-8") as f:
+                    date = json.load(f)
+            except:
+                pass
+
