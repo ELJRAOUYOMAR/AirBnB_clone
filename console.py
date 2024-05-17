7e7b423f9371133a762039b68108d27bd0dd07b0
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-17 20:13:43 +0100

    7. Console 0.1

--- console.py
+++ console.py
@@ -12,7 +12,7 @@ class HBNBCommand(cmd.Cmd):
     """
     """
     prompt = "(hbnb) "
-    classes = {
+    class_mapping = {
         "BaseModel": BaseModel,
         "User": User
     }
@@ -36,19 +36,145 @@ class HBNBCommand(cmd.Cmd):
         -If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ create MyModel)
         
         shlex class makes it easy to write lexical analyzers for simple
-        syntaxes resembling that of the Unix shell.s
+        syntaxes resembling that of the Unix shell.
         """
         
         args = shlex.split(arg)
-
+        print(args)
         if not args:
             print("** class name missing **")
-        elif args[0] not in self.classes: 
+        elif args[0] not in self.class_mapping: 
             print("** class doesn't exist **")
         else:
-            new_instance = eval(f"{args[0]}()")
+            """new_instance = eval(f"{args[0]}()")"""
+            new_instance = self.class_mapping[args[0]]()
             storage.save()
             print(new_instance.id)
 
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class name and id.
+        Ex: $ show BaseModel 1234-1234-1234
+        -If the class name is missing, print ** class name missing ** (ex: $ show)
+        -If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ show MyModel)
+        -If the id is missing, print ** instance id missing ** (ex: $ show BaseModel)
+        -If the instance of the class name doesn’t exist for the id, print ** no instance 
+         found ** (ex: $ show BaseModel 121212)
+        """
+        args = shlex.split(arg)
+
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in self.class_mapping:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{args[0]}.{args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
+    
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (save the change into the JSON file).
+        Ex: $ destroy BaseModel 1234-1234-1234
+        -If the class name is missing, print ** class name missing ** (ex: $ destroy)
+        -If the class name doesn’t exist, print ** class doesn't exist ** (ex:$ destroy MyModel)
+        -If the id is missing, print ** instance id missing ** (ex: $ destroy BaseModel)
+        -If the instance of the class name doesn’t exist for the id, 
+         print ** no instance found ** (ex: $ destroy BaseModel 121212)
+        """
+        args = shlex.split(arg)
+
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in self.class_mapping:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            objs = storage.all()
+            key = f"{args[0]}.{args[1]}"
+            if key not in objs:
+                print("** no instance found **")
+            else:
+                del objs[key]
+                storage.save()
+    
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the class name.
+        Ex: $ all BaseModel or $ all
+        -The printed result must be a list of strings (like the example below)
+        -If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ all MyModel)
+        """
+        objs = storage.all()
+        args = shlex.split(arg)
+
+        if len(args) == 0:
+            for key, value in objs.items():
+                print(str(value))
+        elif args[0] not in self.class_mapping:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objs.items():
+                if key.split('.')[0] == args[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute (save the change into the JSON file).
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+        - If the class name is missing, print ** class name missing **
+        - If the class name doesn’t exist, print ** class doesn't exist **
+        - If the id is missing, print ** instance id missing **
+        - If the instance of the class name doesn’t exist for the id, print ** no instance found **
+        - If the attribute name is missing, print ** attribute name missing **
+        - If the value for the attribute name doesn’t exist, print ** value missing **
+        """
+        args = shlex.split(arg)
+        
+        if len(args) < 1:
+            print("** class name missing **")
+            return
+        if args[0] not in self.class_mapping:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        class_name, instance_id, attribute_name, attribute_value = args[0], args[1], args[2], args[3]
+        
+        """Find the instance"""
+        objs = storage.all()
+        key = f"{class_name}.{instance_id}"
+        
+        if key not in objs:
+            print("** no instance found **")
+            return
+
+        instance = objs[key]
+        
+        """Cast the attribute value to the correct type"""
+        attribute_type = type(getattr(instance, attribute_name, str))
+        if attribute_type is int:
+            attribute_value = int(attribute_value)
+        elif attribute_type is float:
+            attribute_value = float(attribute_value)
+        
+        setattr(instance, attribute_name, attribute_value)
+        instance.save()
+        print(instance)
+        
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
