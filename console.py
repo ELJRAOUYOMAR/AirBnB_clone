b1ba4ac4f823482d1dec4617110323c3f1db2509
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-17 17:52:44 +0100

    7. Console 0.1: create command

--- console.py
+++ console.py
@@ -3,13 +3,19 @@
 
 """
 import cmd
+import shlex
 from models.base_model import BaseModel
-
+from models.user import User
+from models import storage 
 
 class HBNBCommand(cmd.Cmd):
     """
     """
     prompt = "(hbnb) "
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User
+    }
 
     def do_quit(self, argument):
         """command to exit the program"""
@@ -23,13 +29,26 @@ class HBNBCommand(cmd.Cmd):
         """empty line + ENTER shouldn’t execute anything"""
         pass
 
-    def create(self):
+    def do_create(self, arg):
         """Creates a new instance of BaseModel, saves it (to the JSON file) 
         and prints the id. Ex: $ create BaseModel
         -If the class name is missing, print ** class name missing ** (ex: $ create)
         -If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ create MyModel)
+        
+        shlex class makes it easy to write lexical analyzers for simple
+        syntaxes resembling that of the Unix shell.
         """
-        base_model = BaseModel()
+        
+        args = shlex.split(arg)
+
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in self.classes: 
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(f"{args[0]}()")
+            storage.save()
+            print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
