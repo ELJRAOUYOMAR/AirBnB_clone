5b243b6d1af5a01ba25be432efae4b86c3f86b91
oneameeeena <aminaouaqqaourahou@gmail.com>
2024-05-19 05:26:36 -0700

    ARBNB

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 command line code.
-you can use your own command line for CRUD operation, that's all!ueeeeeeeeeeep
+you can use your own command line for CRUD operation, that's all!u
 """
 import cmd
 import shlex
